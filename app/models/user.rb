f3a4877e6c906252082532d7ee8990e973df9580
Naoki.K <nyaoking1012@yahoo.co.jp>
2020-11-15 09:05:47 +0000

    kadai2-first

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,14 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    has_many :favorites
+    has_many :likeposts, through: :favorites, source: :micropost
+    # has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+    # has_many :likeposts, through: :reverses_of_favorite, source: :user
+    
+    
+    
+    
     def follow(other_user)
         unless self == other_user
           self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,5 +38,20 @@ class User < ApplicationRecord
       def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
       end
-    
+      
+      #お気に入り登録・解除
+      def like(other_micropost)
+        self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+      end
+      
+      def unlike(other_micropost)
+        favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+        favorite.destroy if favorite
+      end
+      
+      #お気に入りのポストか判定
+      def likepost?(other_micropost)
+        self.likeposts.include?(other_micropost)
+      end
+      
 end
